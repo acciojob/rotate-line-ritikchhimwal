375bc09d2f112384a9dd9f478d529c80bef9f0c2
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-10-29 15:17:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const line = document.getElementById("line");
+let angle = 0;
+
+function rotateLine() {
+  angle += 2; // Increase angle by 2 degrees every call
+  line.style.transform = `rotate(${angle}deg) translate(-50%, -50%)`;
+}
+
+setInterval(rotateLine, 20)
